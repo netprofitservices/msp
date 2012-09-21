4a8b25a101b57b1e2e8946c721b4e172ecca4d13
Ella Ip <ella@elladesign.com>
2012-09-21 11:59:57 -0500

    new networkty page, img, css

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -538,7 +538,7 @@ class IndexController extends Zend_Controller_Action
 		// Form processing is handled by the newsletter/signup action
     }
 	
-	public function networkthankyou2()
+	public function networkthankyou2Action()
     {
 		
     }
